49daaa3c1dd98a70d0db55679af9752074e7424d
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-06-14 12:12:42 +0300

    Errors & Stack Traces
    This was one of my favorite lessons, shifting my perspective on what I used to be previousle afraid of on the terminal; Errors.Hoping to get better and better at this in the future.

--- errors.js
+++ errors.js
@@ -2,7 +2,7 @@
 
 const snackSelection = 'Pretzels';
 
-if (snackSelection = 'Pretzels') {
+if (snackSelection === 'Pretzels') {
   console.log("That'll be $1, please!");
 }
 
@@ -17,4 +17,7 @@ function second () {
   third();
 }
 
+function third () {
+  console.log(`You've come to the end of the functions!`)
+}
 first();
